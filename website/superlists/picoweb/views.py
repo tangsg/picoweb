ae798bad90cebe410ae9c49c9767d11d307006f5
tangsg <tangsg@93bfd0e2-9e34-4fdc-9331-d9eb2cac3533>
2016-06-13 15:23:09 +0000

    add picoweb static

--- website/superlists/picoweb/views.py
+++ website/superlists/picoweb/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, redirect
+from picoweb.models import Product, Photo
 
-# Create your views here.
+def index(request):
+	products_list = Product.objects.all()
+	return render_to_response('index.html', locals())
+
+
