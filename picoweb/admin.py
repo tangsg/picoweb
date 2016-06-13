f37bf2801c00f604940964aade67842c518dfc49
tangsg <tangsg@93bfd0e2-9e34-4fdc-9331-d9eb2cac3533>
2016-06-13 15:23:09 +0000

    add picoweb static

--- picoweb/admin.py
+++ picoweb/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from picoweb.models import Product, Photo
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Photo)
--- picoweb/models.py
+++ picoweb/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+	title = models.CharField(max_length=100)
+	model = models.CharField(max_length=20)
+	usedmodel = models.CharField(max_length=20)
+    	features = models.TextField(default='')
+    	watt_normal = models.IntegerField()
+    	watt_peak = models.IntegerField()
+    	vin_min = models.IntegerField()
+    	vin_max = models.IntegerField()
+    	pcb_length = models.FloatField()
+    	pcb_width = models.FloatField()
+    	pcb_height = models.FloatField()
+    	input_connector = models.CharField(max_length=50)
+
+	class meta:
+		ordering = ['model']
+	
+	def __unicode__(self):
+		return self.title
+
+
+class Photo(models.Model):
+	product = models.ForeignKey(Product)
+    	title = models.CharField(max_length=100)
+	size = models.CharField(max_length=100, default='750x500')
+    	image = models.ImageField(upload_to='photos')
+    
+    	class meta:
+    		ordering = ["title"]
+    	
+    	def __unicode__(self):
+    		return self.title    
