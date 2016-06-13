c67b0f8d83b8c34e3a61b6fcb583449a512e42dc
tangsg <tangsg@93bfd0e2-9e34-4fdc-9331-d9eb2cac3533>
2016-06-13 15:24:54 +0000

    change

--- website/superlists/superlists/settings.py
+++ website/superlists/superlists/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'picoweb',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -102,3 +103,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../media'))
+
