ebd8b44f2bafc1103e8c53661b19d0a3e2993fd9
tangsg <tangsg@93bfd0e2-9e34-4fdc-9331-d9eb2cac3533>
2016-06-13 15:24:54 +0000

    change

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/', include('lists.urls')),
-    url(r'^pico/', include('picoweb.urls')),
-    #url(r'^admin/', include(admin.site.urls)),
+    #url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^$', 'picoweb.views.index', name='index'),
+    #url(r'^lists/', include('lists.urls')),
+    #url(r'^pico/', include('picoweb.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
