8e3af0243e0eb756b53b725519ea1a684632e59a
tangsg <tangsg@93bfd0e2-9e34-4fdc-9331-d9eb2cac3533>
2016-06-19 02:32:58 +0000

    commit codes before moving on to mobirise booststrap

--- picoweb/models.py
+++ picoweb/models.py
@@ -1,35 +1,54 @@
 from django.db import models
 
-
 class Product(models.Model):
-	title = models.CharField(max_length=100)
-	model = models.CharField(max_length=20)
-	usedmodel = models.CharField(max_length=20)
-    	features = models.TextField(default='')
-    	watt_normal = models.IntegerField()
-    	watt_peak = models.IntegerField()
-    	vin_min = models.IntegerField()
-    	vin_max = models.IntegerField()
-    	pcb_length = models.FloatField()
-    	pcb_width = models.FloatField()
-    	pcb_height = models.FloatField()
-    	input_connector = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    model = models.CharField(max_length=20)
+    usedmodel = models.CharField(max_length=20)
+    features = models.TextField(default='')
+    watt_normal = models.IntegerField()
+    watt_peak = models.IntegerField()
+    vin_min = models.IntegerField()
+    vin_max = models.IntegerField()
+    pcb_length_mm = models.FloatField()
+    pcb_width_mm = models.FloatField()
+    pcb_height_mm = models.FloatField()
+    input_connector = models.CharField(max_length=50)
+    price_usd = models.FloatField()
+    discount_off_percent = models.FloatField()
 
-	class meta:
-		ordering = ['model']
-	
-	def __unicode__(self):
-		return self.title
+    class meta:
+        ordering = ['model']
+
+    def __unicode__(self):
+        return self.title
 
 
 class Photo(models.Model):
-	product = models.ForeignKey(Product)
-    	title = models.CharField(max_length=100)
-	size = models.CharField(max_length=100, default='750x500')
-    	image = models.ImageField(upload_to='photos')
-    
-    	class meta:
-    		ordering = ["title"]
-    	
-    	def __unicode__(self):
-    		return self.title    
+    product = models.ForeignKey(Product)
+    title = models.CharField(max_length=100)
+    size = models.CharField(max_length=100, default='750x500')
+    image = models.ImageField(upload_to='photos')
+
+    class meta:
+        ordering = ["title"]
+
+    def __unicode__(self):
+        return self.title
+
+
+class Download(models.Model):
+    product = models.ForeignKey(Product)
+    title = models.CharField(max_length=100)
+    file = models.FileField(upload_to='downloads')
+
+    class meta:
+        ordering = ["title"]
+
+    def __unicode__(self):
+        return self.title
+
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    message = models.TextField()
--- picoweb/views.py
+++ picoweb/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render_to_response, redirect
-from picoweb.models import Product, Photo
+from django.shortcuts import render, render_to_response, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from picoweb.models import Product, Photo, Download, Feedback
+
 
 def index(request):
-	products_list = Product.objects.all()
-	return render_to_response('index.html', locals())
+    product_list = Product.objects.all()
+    # print product_list
+    return render(request, 'index.html', {'product_list': product_list})
 
 
+@csrf_exempt
+def proc_feedback(request):
+    new_name = request.POST['name2']
+    new_email = request.POST['email2']
+    new_message = request.POST['message2']
+    Feedback.objects.create(name=new_name, email=new_email, message=new_message)
+    return HttpResponse("OK")
